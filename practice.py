f1eb67e6dc4e574047dbb77a81fff5c9506974f7
riya413 <riyanawaz79@gmail.com>
2025-07-24 03:21:14 -0700

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,138 @@
+
+
+# a = int(input("enter first  number:"))
+# b = int(input("enter second number:"))
+# c = int(input("enter third number:"))
+
+# if(a >= b and a >=c):
+#     print("first numder ids largest,a")
+# elif(b >=c):
+#     print("second number is largest,b")
+# else:
+#     print("third is largest",c)
+# day = 4
+# match day:
+#   case 1:
+#     print("Monday")
+#   case 2:
+#     print("Tuesday")
+#   case 3:
+#     print("Wednesday")
+#   case 4:
+#     print("Thursday")
+#   case 5:
+#     print("Friday")
+#   case 6:
+#     print("Saturday")
+#   case 7:
+#     print("Sunday")
+i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# i = 1
+# def my_function():
+#   print("Hello from a function")
+
+# my_function()
+# def my_function(fname):
+#   print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+#marks = [23,54,65,89,90,78]
+#print(len(marks))
+#print(marks[1:4])
+# thislist = ["apple", "banana", "cherry"]
+
+# thislist.append("orange")
+
+# print(thislist)
+# list = ['apple','banana','orange','kiwi']
+# list2 = ['apple','banana','orange','kiwi']
+# list1.extend(list2)
+# print(list1)
+# for x in list:
+#     print(x)
+# list = ['apple','banana','grapes','chery']
+# i = 0
+# while i < len(list):
+#     print(list[i])
+#     i = i+1
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if x != "apple"]
+
+# print(newlist)
+
+# list = ["apple", "banana", "cherry", "kiwi", "mango"]
+# listnew = [x for x in fruits if x!="banana"]
+# print(listnew)
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+
+# thislist.sort()
+
+# print(thislist)
+# def myfunc(n):
+#   return abs(n - 50)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key = myfunc)
+# print(thislist)
+
+
+
+
+
+# dict = {
+#     " brand" : "ford",
+#      "model" : "kismshow",
+#      "year" : 1976,
+#      "year" : 2020
+# }
+# print(dict)
+
+                            # practice 
+# dict1 = dict(name = "amna",age = 22, country = " gilgit")
+# x = dict1["country"]
+# print (x)
+# car = {
+#     "brand" : "bughati",
+#     "model" : "kiwi",
+#     "year"  : "1976"
+# }
+# x = car.keys()
+# print(x)
+# car["color"] = "white"
+# print (x)
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.items()
+
+# print(x) #before the change
+
+# car["year"] = 2020
+
+# print(x) #after the change
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# if "model" in thisdict:
+#   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+# dire = {
+#   "flag" : "eagle",
+#   "jump" : "eager",
+#   "year" : 1678
+# }
+# if "jump" in dire:
+#   print("yes,'jump' is one of the keys in the dire dictionary")
